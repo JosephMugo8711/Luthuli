8f2b88705ad50ed6a56a2cb1a41225ab3eb8f13e
wamiti8711@gmail.com <wamiti8711@gmail.com>
2024-01-26 20:56:36 +0300

    Feat: productdetails

--- app/components/nav/navbar.tsx
+++ app/components/nav/navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
            <div className="py-4 border-b-[1px]">
             <Container>
                     <div className="flex items-center justify-between gap-3 md-gap-0">
-                        <Link href="/" className={`${redressed.className} font-bold text-2xl`}>Petinder</Link>
+                        <Link href="/" className={`${redressed.className} font-bold text-2xl`}>LuthuliPhones</Link>
                         <div className="hidden md:block">Search</div>
                         <div className="flex items-center gap-8 md-gap-12">
                             <div>CartCount</div>
